3bd53f02e4ed994b89841c34bc387a4e27c61b38
Jaehan-Kevin-Kim <jhkevin21@gmail.com>
2021-10-04 09:54:18 -0600

    Setup initial login and signup routes

--- front/pages/LogIn/index.tsx
+++ front/pages/LogIn/index.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React from 'react'
 
 const LogIn = () => {
-return (
-    <div>LogIn Page</div>
-)
+    return (
+        <div>
+           
+        <h1>why?</h1>
+        <div>LogIn Page</div>
+     
+        </div>
+    )
 }
 
 export default LogIn;
+
+
+// import React from 'react';
+
+// const LogIn = () => {
+//     return (
+//         <>
+//         <h1>why?</h1>
+//         <div>LogIn Page</div>
+//         </>
+//     )
+// }
+
+// export default LogIn;
+
