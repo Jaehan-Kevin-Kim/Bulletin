2492ebe3c61e0ce925dbafc82ed516927d9d413b
Jaehan-Kevin-Kim <jhkevin21@gmail.com>
2022-01-08 15:56:06 -0700

    Custom Scrollbar (react-custom-scrollbars) 추가

--- front/pages/DirectMessage/index.tsx
+++ front/pages/DirectMessage/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react'
+import React, { useCallback } from 'react';
 import { Container, Header } from './styles';
 import gravatar from 'gravatar';
 import useSWR from 'swr';
@@ -11,50 +11,52 @@ import { transpileModule } from 'typescript';
 import axios from 'axios';
 import { IDM } from '@typings/db';
 
-
-
 const DirectMessage = () => {
+  const { workspace, id } = useParams<{ workspace: string; id: string }>();
+  const { data: userData } = useSWR(`/api/workspaces/${workspace}/users/${id}`, fetcher);
+  const { data: myData } = useSWR(`/api/users`, fetcher);
+  const [chat, onChangeChat, setChat] = useInput('');
 
-    const { workspace, id } = useParams<{ workspace: string, id: string }>();
-    const { data: userData } = useSWR(`/api/workspaces/${workspace}/users/${id}`, fetcher)
-    const { data: myData } = useSWR(`/api/users`, fetcher)
-    const [chat, onChangeChat, setChat] = useInput('')
+  const { data: chatData, mutate: mutateChat } = useSWR<IDM[]>(
+    `/api/workspaces/${workspace}/dms/${id}/chats?perPage=20&page=1`,
+    fetcher,
+  );
 
-    const { data: chatData, mutate: mutateChat } = useSWR<IDM[]>(
-        `/api/workspaces/${workspace}/dms/${id}/chats?perPage=20&page=1`, fetcher,
-    );
+  const onSubmitForm = useCallback(
+    (e) => {
+      e.preventDefault();
+      console.log(chat);
+      // setChat('');
+      console.log('submit');
+      if (chat?.trim()) {
+        axios
+          .post(`/api/workspaces/${workspace}/dms/${id}/chats`, {
+            content: chat,
+          })
+          .then(() => {
+            mutateChat();
+            setChat('');
+          })
+          .catch(console.error);
+      }
+    },
+    [chat],
+  );
 
-    const onSubmitForm = useCallback((e) => {
-        e.preventDefault();
-        console.log(chat);
-        // setChat('');
-        console.log("submit")
-        if (chat?.trim()) {
-            axios.post(`/api/workspaces/${workspace}/dms/${id}/chats`, {
-                content: chat,
-            })
-                .then(() => {
-                    mutateChat();
-                    setChat('');
-                })
-                .catch(console.error)
-        }
-    }, [chat])
+  if (!userData || !myData) {
+    return null;
+  }
 
-    if (!userData || !myData) {
-        return null;
-    }
-
-    return (
-        <Container>
-            <Header>
-                <img src={gravatar.url(userData.email, { s: '24px', d: 'retro' })} alt="{userData.nickname}" />
-                <span>{userData.nickname}</span>
-            </Header>
-            <ChatList />
-            <ChatBox placeholder="placeholder" chat={chat} onChangeChat={onChangeChat} onSubmitForm={onSubmitForm} />
-        </Container>
-    )
-}
+  return (
+    <Container>
+      <Header>
+        <img src={gravatar.url(userData.email, { s: '24px', d: 'retro' })} alt="{userData.nickname}" />
+        <span>{userData.nickname}</span>
+      </Header>
+      <ChatList chatData={chatData} />
+      <ChatBox placeholder="placeholder" chat={chat} onChangeChat={onChangeChat} onSubmitForm={onSubmitForm} />
+    </Container>
+  );
+};
 
 export default DirectMessage;
